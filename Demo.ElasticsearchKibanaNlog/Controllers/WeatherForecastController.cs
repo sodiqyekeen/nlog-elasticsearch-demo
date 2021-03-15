94e08b2e9a4adabbe13b2b2dfaf5c7b13aff8b6d
sodiqyekeen <sodiq.yekeen.o@gmail.com>
2021-03-15 16:11:29 +0300

    implements nlog with elasticsearch and kibana

--- Demo.ElasticsearchKibanaNlog/Controllers/WeatherForecastController.cs
+++ Demo.ElasticsearchKibanaNlog/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace Demo.ElasticsearchKibanaNlog.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("About to get weather forecasts.");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- Demo.ElasticsearchKibanaNlog/Program.cs
+++ Demo.ElasticsearchKibanaNlog/Program.cs
@@ -6,14 +6,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace Demo.ElasticsearchKibanaNlog
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Statrting main...");
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "An exception occurred preventing the application from starting.");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +38,12 @@ namespace Demo.ElasticsearchKibanaNlog
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
